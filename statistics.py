a3f8d14c5fe3e8474a0eaeb41c8602a339964a42
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-10-13 13:57:28 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,13 +1,10 @@
-import math
 def calculateStats(numbers):
-  cs = {}
-  cs["avg"] = math.nan
-  cs["max"] = math.nan
-  cs["min"] = math.nan
-
-  if len(numbers) > 0:
-    cs["avg"] = sum(numbers) / len(numbers)
-    cs["max"] = max(numbers)
-    cs["min"] = min(numbers)
-    
-  return
+  dic = {"avg" : 0, "max" : 0, "min" : 0}
+  if len(numbers) != 0:
+    num_sum = sum(numbers)
+    dic["avg"] = num_sum / len(numbers)
+    dic["max"] = max(numbers)
+    dic["min"] = min(numbers)
+  else:
+    print("Input is empty")
+  return dic
