6c150612ba31636506aa22e70c7ca977915d3b11
DeepthiThotatcq <115614126+DeepthiThotatcq@users.noreply.github.com>
2022-10-18 14:58:31 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,1 +1,10 @@
-
+def calculateStats(numbers):
+  dic = {"avg" : 0, "max" : 0, "min" : 0}
+  if len(numbers) != 0:
+    num_sum = sum(numbers)
+    dic["avg"] = num_sum / len(numbers)
+    dic["max"] = max(numbers)
+    dic["min"] = min(numbers)
+  else:
+    print("Input is empty")
+  return dic
